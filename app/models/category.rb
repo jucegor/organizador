8cecedcdcebec741573983266eda0fac75514e33
Julio Gonzalez <jucegor@gmail.com>
2022-03-21 15:04:56 -0600

    Repaso exitoso de scaffold, intro a devise

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
   has_many :tasks
+  # Valida que los campos name y description no se admitan vacios.
   validates :name, :description, presence: true
+
+  # Validación de unicidad
+  validates :name, uniqueness: { case_sensitive: false }
+  # Usando el ejemplo de platzi no funciona, funciona con sensitive
+  # https://stackoverflow.com/questions/63165940/warnings-about-case-sensitive-cant-be-erased
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,20 @@
 class Task < ApplicationRecord
   belongs_to :category
+  validates :name, :description, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  # lineas 3 y 4 se copian directamente desde category, funcionan igual
+
+
+  # Validación personalizada
+  # Se escribe en singular como "validate", en lugar de "validates"
+  # Generando una función personalizada, podemos crear validaciones personalizadass
+
+  validate :due_date_validity
+
+  def due_date_validity
+    return if due_date.blank?
+    return if due_date > Date.today
+    #Para agregar mensajes de error relativos a la validación
+    errors.add(:due_date, "La fecha no puede estar en el pasado o en blanco.")
+  end
 end
